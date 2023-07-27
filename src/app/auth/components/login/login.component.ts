f48d62a036e172811a31e9d8fbdbfcc568c2eb89
HenryAprendo <hjsmipuc@gmail.com>
2023-07-27 09:39:47 -0500

    Maquetado y funcionamiento basico del formulario de login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styles: [
   ]
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+
+  loginForm!:FormGroup;
+
+  constructor(){
+    this.formBuilder();
+  }
+
+  private formBuilder(){
+    this.loginForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+      }
+    );
+  }
+
+  get emailField(){
+    return this.loginForm?.get('email');
+  }
+  get passwordField(){
+    return this.loginForm?.get('password');
+  }
 
 }
+
